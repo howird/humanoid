8e1990cce7915d2bdeb97ebc4136b702c613f2e2
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-09-20 13:07:43 -0400

    stop supporting rocm (for now)

--- scripts/check_torch.py
+++ scripts/check_torch.py
@@ -3,26 +3,34 @@ import sys
 
 import torch
 
+
 def make_tensor(device="cuda"):
     return torch.rand((10, 10), device=device)
 
-if os.environ["PIXI_ENVIRONMENT_NAME"] == "rocm":
-    assert torch.cuda.is_available() and (torch.version.hip is not None), "HIP is not available"
-    print(make_tensor())
-    print("ROCm is available, in rocm environment as expected")
 
-if os.environ["PIXI_ENVIRONMENT_NAME"] == "cuda":
-    assert torch.cuda.is_available() and (torch.version.cuda is not None), "CUDA is not available"
+def check_cuda():
+    assert torch.cuda.is_available() and (
+        torch.version.cuda is not None
+    ), "CUDA is not available"
     print(make_tensor())
     print("CUDA is available, in cuda environment as expected")
 
-if os.environ["PIXI_ENVIRONMENT_NAME"] == "default":
+
+def check_cpu():
     assert not torch.cuda.is_available(), "CUDA is available, in default environment"
-    print(make_tensor())
+    print(make_tensor("cpu"))
     print("CUDA is not available, in default environment as expected")
 
-print("\nHello from train.py!")
-print("Environment you are running on:")
-print(os.environ["PIXI_ENVIRONMENT_NAME"])
-print("Arguments given to the script:")
-print(sys.argv[1:])
+
+if __name__ == "__main__":
+    print(f"Hello from {__file__}!")
+    print(f"Args: {sys.argv[1:] if len(sys.argv) > 1 else 'None'}")
+
+    if "PIXI_ENVIRONMENT_NAME" in os.environ:
+        print(f'Running on pixi env: {os.environ["PIXI_ENVIRONMENT_NAME"]}')
+
+    if sys.argv[1] == "--cuda":
+        check_cuda()
+
+    if sys.argv[1] == "--cpu":
+        check_cpu()
