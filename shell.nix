d7b097a3a138dccd363a3bb412fef168e6add36b
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-09-19 21:05:27 -0400

    fix nix-shell

--- shell.nix
+++ shell.nix
@@ -1,41 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  name = "cuda";
-  packages = with pkgs; [ 
-    # git gitRepo
-    # gnupg
-    # autoconf
-    # curl
-    # procps
-    # gnumake
-    # util-linux
-    # m4
-    # gperf
-    # unzip
-    # zlib 
+{
+  pkgs ? import <nixpkgs> {},
+  lib ? pkgs.lib,
+}:
+pkgs.mkShell rec {
+  name = "isaacgym";
+  packages = with pkgs; [
+    cudatoolkit
+    linuxPackages.nvidia_x11
 
-    # cudatoolkit linuxPackages.nvidia_x11
-    # libGLU libGL
-    # xorg.libXi xorg.libXmu freeglut
-    # xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr
-    # ncurses5
-    # stdenv.cc
-    # binutils
-
-    # pixi
-
-    # cmake
-    # mesa
-    # vulkan-loader
-    # vulkan-headers
-    # vulkan-tools
+    pixi
   ];
-  runScript = "${pkgs.zsh}/bin/zsh";
-  # runScript = "${pkgs.bash}/bin/bash";
-  # profile = ''
-  #   export CUDA_PATH=${pkgs.cudatoolkit}
-  #   # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
-  #   export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-  #   export EXTRA_CCFLAGS="-I/usr/include"
-  # '';
+
+  LD_LIBRARY_PATH =
+    lib.makeLibraryPath (with pkgs; [linuxPackages.nvidia_x11])
+    + ":${builtins.toString ./.}/.pixi/envs/${name}/lib";
+
+  shellHook = ''
+    pixi shell -e ${name}
+  '';
 }
