269889bd9641c091e96e3bcdc08e7edc3bff9d73
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-10-28 11:29:28 -0400

    refactor debugpy

--- scripts/check_torch.py
+++ scripts/check_torch.py
@@ -1,6 +1,4 @@
-import os
 import sys
-
 import torch
 
 
@@ -9,28 +7,33 @@ def make_tensor(device="cuda"):
 
 
 def check_cuda():
-    assert torch.cuda.is_available() and (
+    make_tensor()
+    return torch.cuda.is_available() and (
         torch.version.cuda is not None
-    ), "CUDA is not available"
-    print(make_tensor())
-    print("CUDA is available, in cuda environment as expected")
+    )
+
+
+def check_rocm():
+    make_tensor()
+    return torch.cuda.is_available() and (
+        torch.version.hip is not None
+    )
 
 
 def check_cpu():
-    assert not torch.cuda.is_available(), "CUDA is available, in default environment"
-    print(make_tensor("cpu"))
-    print("CUDA is not available, in default environment as expected")
+    make_tensor("cpu")
+    return not torch.cuda.is_available()
 
 
 if __name__ == "__main__":
-    print(f"Hello from {__file__}!")
-    print(f"Args: {sys.argv[1:] if len(sys.argv) > 1 else 'None'}")
+    print(f"Hello from {__file__}! Args: {sys.argv[1:] if len(sys.argv) > 1 else 'None'}")
+    print(f"")
 
-    if "PIXI_ENVIRONMENT_NAME" in os.environ:
-        print(f'Running on pixi env: {os.environ["PIXI_ENVIRONMENT_NAME"]}')
-
-    if sys.argv[1] == "--cuda":
-        check_cuda()
-
-    if sys.argv[1] == "--cpu":
-        check_cpu()
+    if check_cuda():
+        print("CUDA is available")
+    elif check_rocm():
+        print("ROCm is available")
+    elif check_cpu():
+        print("CPU is available")
+    else:
+        raise ValueError("CUDA, ROCm, CPU all not available")
